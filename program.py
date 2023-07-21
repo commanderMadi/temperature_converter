975ceb4147ab6c2efbe4728a8fc64bf7b8e3bbf8
Ahmed Magdy <ahmed.magdy@woven-planet.global>
2023-07-21 13:05:37 +0900

    initial commit

---
+++ program.py
@@ -0,0 +1,8 @@
+def convertCelsiusToFahrenheit():
+    print("Enter the temperature in Celsius:")
+    celsius = input()
+    celsius = int(celsius)
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"The Celsius temperature {celsius} you entered is {fahrenheit} in Fahrenheit.")
+
+
