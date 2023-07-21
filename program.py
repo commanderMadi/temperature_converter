0571083574a24245e6192cff55bef47ad6ae7d07
Ahmed Magdy <madimagdy@gmail.com>
2023-07-21 13:35:07 +0900

    add code to convert fahrenheit to celsius

--- program.py
+++ program.py
@@ -3,6 +3,14 @@ def convertCelsiusToFahrenheit():
     celsius = input()
     celsius = int(celsius)
     fahrenheit = (celsius * 9/5) + 32
-    print(f"The Celsius temperature {celsius} you entered is {fahrenheit} in Fahrenheit.")
+    print(
+        f"The Celsius temperature {celsius} you entered is {fahrenheit} in Fahrenheit.")
 
 
+def convertFahrenheitToCelsius():
+    print("Enter the temperature in Fahrenheit:")
+    fahrenheit = input()
+    fahrenheit = int(fahrenheit)
+    celsius = 5/9 * (fahrenheit - 32)
+    print(
+        f"The Fahrenheit temperature {fahrenheit} you entered is {celsius} in Celsius.")
